2dfd1c8cdfca01da0341f4bb5d0525eee1bd09e1
WIN10 <WIN10@LAPTOP-CN9CH0G9>
2022-07-19 23:50:15 +0530

    [bhavna]:added new project

---
+++ src/Sampl1/demo1.java
@@ -0,0 +1,17 @@
+package Sampl1;
+
+public class demo1 {
+
+	public static void main(String[] args)
+	{
+
+System.out.println("hii");
+
+System.out.println("hii");
+
+System.out.println("hello");
+
+System.out.println("hello");
+	}
+
+}
